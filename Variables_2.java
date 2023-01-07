a9b7c566a5062ee7e0f42237c6bbbb2b99d84c60
Shreya K <95535050+ShreyaK2710@users.noreply.github.com>
2023-01-07 12:37:57 +0530

    Create Variables_2.java

---
+++ Variables_2.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {2,5,7,4,9};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
