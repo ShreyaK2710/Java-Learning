023a7b30ce69fb062e4ce515b36f0a13ae117a95
Shreya K <95535050+ShreyaK2710@users.noreply.github.com>
2023-01-07 14:18:10 +0530

    Create arithmetic_operations.java

---
+++ arithmetic_operations.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int add = 1+1;
+        int sub = 1-2;
+        int mul = 2*2;
+        double div = (double)3/(double) 10;
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+    }
+}
