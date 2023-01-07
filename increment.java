e35e65be1d2c9e6f2f9a3ecd3160f4c529af23e3
Shreya K <95535050+ShreyaK2710@users.noreply.github.com>
2023-01-07 14:20:14 +0530

    Create increment.java

---
+++ increment.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int x=1;
+        x++;
+        int y=2;
+        ++y;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
